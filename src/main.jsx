0673082ae54b4cf4010984f9a6253d4d664b9aaa
haneefshai786-boop <haneefshai786@gmail.com>
2025-12-24 21:31:06 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { CartProvider } from "./context/CartContext";
 import { UserProvider } from "./context/UserContext";
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
