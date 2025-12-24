1e274049c9ab8e0897b48a39760b8ce4963479be
haneefshai786-boop <haneefshai786@gmail.com>
2025-12-24 20:58:21 +0530

    Update axiosConfig.js

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3500/api",
+  baseURL: "https://solebackend.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
